44b138cb26452b2c050883423e125fd1fbd0a226
petro <feyk12petro@gmail.com>
2025-02-07 00:06:55 +0200

    add

---
+++ smartShafa/smartShafa.ino
@@ -0,0 +1,39 @@
+#include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
+#include "wifiConnect.h"
+#include "mainControll.h"
+#include "clock.h"
+#include "config.h"
+#include "ota.h"
+#include "pin.h"
+#include "security.h"
+#include "transistor.h"
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const char *WIFI_SSID = SSID;
+const char *WIFI_PASSWORD = PASSOWORD;
+
+const char *ntpServer1 = NTP_SERVER1;
+const char *ntpServer2 = NTP_SERVER2;
+const long gmtOffset_sec = GTMOFFSET_SEC;
+const int daylightOffset_sec = DAYLIGHTOFFSET;
+
+String apiURL = API_URL;
+String apiKey = API_KEY;
+String latitude = LAT;
+String longitude = LON;
+
+void setup()
+{
+    Serial.begin(115200);
+    connectToWiFi(WIFI_SSID, WIFI_PASSWORD);
+
+    setupOTA("my_esp32", OTA_PIN);
+}
+
+void loop()
+{
+
+    handleOTA();
+}
