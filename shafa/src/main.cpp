73bbdb55b8fd1b5813d23198157b7a6426b0feb7
petro <feyk12petro@gmail.com>
2025-04-02 16:20:34 +0300

    add o

--- shafa/src/main.cpp
+++ shafa/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Keypad.h>
+#include "KeyPadControl.h"
 #include "config.h"
 #include "pin.h"
 #include "transistor.h"
@@ -7,8 +8,10 @@
 void setup()
 {
   Serial.begin(115200);
+  KeyPadControl.keyPadSetup();
 }
 
 void loop()
 {
+  KeyPadControl.keyPadLoop();
 }
