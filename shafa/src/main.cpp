a70ae8e4537cf779d8f6288ca8e022c5eaa90db8
petro <feyk12petro@gmail.com>
2025-04-04 20:01:37 +0300

    littlefs

--- shafa/src/main.cpp
+++ shafa/src/main.cpp
@@ -13,10 +13,47 @@ KeyPadControl keyPadControl(lcd, storage);
 void setup()
 {
   Serial.begin(115200);
+
+  if (!LittleFS.begin(true))
+  {
+    Serial.println("An Error has occurred while mounting LittleFS");
+    return;
+  }
+
+  File file = LittleFS.open("/text.txt", "r");
+  if (!file)
+  {
+    Serial.println("File not found, creating new one...");
+    file = LittleFS.open("/text.txt", "w");
+    if (file)
+    {
+      file.println("1234");
+      file.close();
+      Serial.println("File created");
+    }
+    else
+    {
+      Serial.println("Failed to create file");
+    }
+  }
+
+  if (!file)
+  {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.println("File Content:");
+  while (file.available())
+  {
+    Serial.write(file.read());
+  }
+  file.close();
+
   keyPadControl.keyPadSetup();
   Serial.println("Setup complete");
   pinMode(TRANSISTOR_PIN, OUTPUT);
-  digitalWrite(TRANSISTOR_PIN, LOW); // Вимкнути транзистор
+  digitalWrite(TRANSISTOR_PIN, LOW);
   Serial.println("Transistor pin set to LOW");
 }
 
